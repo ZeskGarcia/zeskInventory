b0ea91731e61ff727d3f171c3c15a69e0ea6f29a
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2023-10-13 17:30:32 +0200

    Update Player.lua

--- Server/Functions/Player.lua
+++ Server/Functions/Player.lua
@@ -12,8 +12,43 @@ Player.Functions.getPlayerInventory = function(playerId)
       xPlayer = Framework.GetPlayerFromId(src)
     end
 
-    local inventoryContent = xPlayer.getInventory()
+    if (xPlayer ~= nil) then
+      local inventoryContent = xPlayer.getInventory()
+  
+      return { success = true, response = inventoryContent }
+    else
+      return { success = false, message = "Player Not Found" }
+    end
+  end
+end
 
-    return inventoryContent
+Player.Functions.removeInventoryItem = function(item, playerId)
+  local xPlayer = nil
+  local src = source
+
+  if (Config.Framework and string.upper(Config.Framework) == "ESX") then
+    if (playerId) then
+      xPlayer = Framework.GetPlayerFromId(playerId)
+    else
+      xPlayer = Framework.GetPlayerFromId(src)
+    end
+    
+    if (xPlayer ~= nil) then
+      local checkItem = xPlayer.getInventoryItem(item)
+      if (checkItem and checkItem.count > 0) then
+        local currentItemCount = checkItem.count
+        xPlayer.removeInventoryItem(item, 1)
+        checkItem = xPlayer.getInventoryItem(item)
+        if (checkItem and (checkItem.count == (currentItemCount - 1))) then
+          return { success = true, message = "Succesfully Removed Item from Player's Inventory" }
+        else
+          return { success = false, message = "Error removing item" }
+        end
+      else
+        return { success = false, message = "Item Not Found" }
+      end
+    else
+      return { success = false, message = "Player Not Found" }
+    end
   end
 end
