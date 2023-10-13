dbc9db0757ff5756b5dec4d32de05666e88ab167
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2023-10-13 17:33:15 +0200

    Update Config.lua

--- Config.lua
+++ Config.lua
@@ -1,4 +1,5 @@
 Config = {
   Framework = "esx", -- currently only ESX
+  FrameworkName = "es_extended", -- Current Framework Name, example: es_extended (ESX Legacy)
   NewVersion = true, -- Enable this if you framework works with the exports function.
 }
