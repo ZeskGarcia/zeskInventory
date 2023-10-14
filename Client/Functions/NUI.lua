491358b1f551e95f41e0e553820d730750b65a20
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2023-10-14 10:18:29 +0200

    Add NUI Function.

---
+++ Client/Functions/NUI.lua
@@ -0,0 +1,12 @@
+-- Send Data to the NUI window.
+-- Function from Jaareet (https://github.com/jaareet)
+
+function sendNuiMessage(action, data)
+    local payload = { type = tostring(action) }
+    if (data and type(data) == 'table') then
+        for k, v in pairs(data) do
+            payload[k] = v
+        end
+    end
+    SendNUIMessage(payload)
+end
