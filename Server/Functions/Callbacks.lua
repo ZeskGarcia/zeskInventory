b0ec418fcc00b30d1cf8f369c7f563994ff560fd
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2023-10-14 15:27:20 +0200

    Add Inventory Callback register

--- Server/Functions/Callbacks.lua
+++ Server/Functions/Callbacks.lua
@@ -1,4 +1,9 @@
-CreateThread(function()
-        Framework.RegisterServerCallback('')
+Shared.Functions.registerServerCallback(GetCurrentResourceName() .. ":getPlayerInventory", function(playerId, invPlayer, cb)
+    local inventory
+    if (invPlayer == nil) then
+        inventory = Player.Functions.getPlayerInventory(playerId)
+    else
+        inventory = Player.Functions.getPlayerInventory(invPlayer)
     end
+    cb(inventory)
 end)
