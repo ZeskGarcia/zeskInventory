33beeb86b87f148060a83cf6916c331146379d9e
Zesk <116458300+ZeskGarcia@users.noreply.github.com>
2023-10-14 15:53:05 +0200

    Remove Item From Player Inventory

--- Client/Functions/Player.lua
+++ Client/Functions/Player.lua
@@ -10,5 +10,32 @@ Player.Functions.OpenInventory = function()
         else
             print("Failed getting player inventory")
         end
-    end, '')
+    end, nil)
+end
+
+Player.Functions.CloseInventory = function()
+    sendNuiMessage('closeInventory', {})
+end
+
+Player.Functions.removeInventoryItem = function(player, item)
+    local targetedPlayer
+    if (player and item) then
+        targetedPlayer = player
+    elseif (player == nil and item) then
+        targetedPlayer = nil
+    else
+        print("Item or Player must be specified")
+        return    
+    end
+    
+    Shared.Functions.triggerServerCallback(GetCurrentResourceName() .. ":removeInventoryItem", function(response)
+        if (response and response.success) then
+            print("Successfully remove item from player inventory")
+        else
+            print("Couldn't Remove the item from the inventory")
+        end
+    end, {
+        targetPlayer = targetedPlayer,
+        item = item
+    })
 end
--- Server/Functions/Callbacks.lua
+++ Server/Functions/Callbacks.lua
@@ -1,9 +1,33 @@
 Shared.Functions.registerServerCallback(GetCurrentResourceName() .. ":getPlayerInventory", function(playerId, invPlayer, cb)
     local inventory
+
     if (invPlayer == nil) then
         inventory = Player.Functions.getPlayerInventory(playerId)
     else
         inventory = Player.Functions.getPlayerInventory(invPlayer)
     end
+
     cb(inventory)
+end)
+
+Shared.Functions.registerServerCallback(GetCurrentResourceName() .. ":removeInventoryItem", function(playerId, invData, cb)
+    local targetPlayer
+    local removeItem
+
+    if (invData and invData.targetPlayer == nil) then
+        targetPlayer = playerId
+    elseif (invData and invData.targetPlayer) then
+        targetPlayer = invData.targetPlayer
+    end
+
+    if (invData and invData.item and invData.item ~= nil) then
+        removeItem = invData.item
+    else
+        print("ITEM Not specified")
+        return
+    end
+
+    local removedItemResponse = Player.Functions.removeInventoryItem(removeItem, targetPlayer)
+
+    cb(removedItemResponse)
 end)
